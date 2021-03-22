cacd63c111a5d16c9175e9b1953f431cb37aec87
MarcelPohl97 <Marcel.Pohl1234@gmx.de>
2021-03-22 22:26:10 +0100

    Update firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 if(!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+        apiKey: "AIzaSyCZw3Pz12wwXSzwTzZgOmyu9cinNq4hfiY",
+        authDomain: "achievely-c9638.firebaseapp.com",
+        projectId: "achievely-c9638",
+        storageBucket: "achievely-c9638.appspot.com",
+        messagingSenderId: "10573835882",
+        appId: "1:10573835882:web:2f3d0fd32b2af24900a413",
+        measurementId: "G-6C8D3KWM7H"
     });
 }
 
